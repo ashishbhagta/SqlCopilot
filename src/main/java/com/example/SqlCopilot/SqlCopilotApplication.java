180ade46070486473ee1bb4fa48e19c6f08b28c1
Ashish Garg <ashishbhagta1996@gmail.com>
2025-08-10 12:13:56 +0530

    Initial commit

---
+++ src/main/java/com/example/SqlCopilot/SqlCopilotApplication.java
@@ -0,0 +1,13 @@
+package com.example.SqlCopilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlCopilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlCopilotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SqlCopilot/SqlCopilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SqlCopilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlCopilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
